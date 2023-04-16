2de37106f2c767766e502aab2eeca91564ab9b3a
Yaser Sharifi <yassersharifi74@gmail.com>
2023-04-16 21:05:42 +0330

    add authentication to swagger

--- src/swagger/definitions/index.js
+++ src/swagger/definitions/index.js
@@ -9,10 +9,25 @@ const createUserParse = YAML.parse(createUser);
 const article = fs.readFileSync(path.join(__dirname, 'article.yaml'), 'utf8');
 const articleParse = YAML.parse(article);
 
+// unauthorized definition
+const unauthorizedDefinition = fs.readFileSync(path.join(__dirname, 'unauthorized.yaml'), 'utf8');
+const unauthorizedDefinitionParse = YAML.parse(unauthorizedDefinition);
+
+// forbidden definition
+const forbiddenDefinition = fs.readFileSync(path.join(__dirname, 'forbidden.yaml'), 'utf8');
+const forbiddenDefinitionParse = YAML.parse(forbiddenDefinition);
+
+// bad request definition
+const badRequestDefinition = fs.readFileSync(path.join(__dirname, 'bad-request.yaml'), 'utf8');
+const badRequestDefinitionParse = YAML.parse(badRequestDefinition);
+
 const definitions = {
     definitions: {
         // ...createUserParse,
         ...articleParse,
+        ...unauthorizedDefinitionParse,
+        ...forbiddenDefinitionParse,
+        ...badRequestDefinitionParse,
     },
 };
 
